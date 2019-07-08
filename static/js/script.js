e413f6a26745a60b04b19d5623501e66a47a90f2
Void <voidpls@protonmail.com>
2019-07-08 18:29:01 +0800

    lazy loading images and avatars

--- static/js/script.js
+++ static/js/script.js
@@ -3,9 +3,40 @@ const config = {
   threshold: 0
 }
 
+const imgs = document.querySelectorAll('[data-src]')
+
+function setSrc (target, newsrc) {
+  target.src = newsrc
+  // if (target.dataset.src)
+  target.removeAttribute('data-src')
+}
+
+function loadFailed (el) {
+  const src = el.src
+  const fallback = el.dataset.fallback
+  setSrc(el, fallback)
+  console.log(fallback)
+  const failedImgs = document.querySelectorAll(`[data-fallback='${fallback}']`)
+  console.log(failedImgs)
+  failedImgs.forEach(i => {
+    // setSrc(i, fallback)
+  })
+  // console.log(el.dataset.fallback)
+  // console.log(el.src)
+}
+
 function preloadImage (target) {
-  target.setAttribute('src', target.dataset.src)
-  target.classList.remove('defer_thumbnail')
+  const targetSrc = target.dataset.src
+  if (!targetSrc) return
+  target.onload = function () {
+    if (target.classList.contains('defer_thumbnail')) {
+      target.classList.remove('defer_thumbnail')
+    }
+    Array.from(imgs)
+      .filter(i => i.getAttribute('data-src') === targetSrc)
+      .map(i => setSrc(i, targetSrc))
+  }
+  setSrc(target, targetSrc)
 }
 
 const observer = new IntersectionObserver(function (entries, self) {
@@ -17,7 +48,7 @@ const observer = new IntersectionObserver(function (entries, self) {
   })
 }, config)
 
-const imgs = document.querySelectorAll('[data-src]')
+console.log(imgs.length)
 imgs.forEach(img => {
   observer.observe(img)
 })
